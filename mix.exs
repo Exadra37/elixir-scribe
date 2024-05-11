6708a63c38661e3c7db5f84292cc7c7900f256fb
Exadra37 <exadra37@gmail.com>
2024-05-11 16:31:37 +0100

    Preparing the package for publication.
    Signed-off-by: Exadra37 <exadra37@gmail.com>

--- mix.exs
+++ mix.exs
@@ -1,12 +1,28 @@
 defmodule ElixirScribe.MixProject do
   use Mix.Project
 
+  # The Elixir version here needs to be equal or greater then the one
+  # used by the Phoenix Framework and Phoenix Installer.
+  @elixir_requirement "~> 1.14"
+  @scm_url "https://github.com/exadra37/elixir-scribe"
+
+  @description """
+  Elixir Scribe - A Mix Task to Generate Elixir and Phoenix Projects
+
+  The Elixir Scribe tool aims to help developers to more easily write clean code in a clean software architecture for enhanced developer experience and productivity.
+  """
+
   def project do
     [
       app: :elixir_scribe,
+      name: "Elixir Scribe",
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: @elixir_requirement,
       start_permanent: Mix.env() == :prod,
+      homepage_url: @scm_url,
+      source_url: @scm_url,
+      description: @description,
+      package: package(),
       deps: deps()
     ]
   end
@@ -18,11 +34,22 @@ defmodule ElixirScribe.MixProject do
     ]
   end
 
+  defp package do
+    [
+      maintainers: ["Paulo Renato (Exadra37)"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @scm_url},
+      files:
+        ~w(lib priv LICENSE.md mix.exs README.md .formatter.exs)
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
