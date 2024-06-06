cad1a5207a80051af4651bf25374e90d3db1fa0b
Exadra37 <exadra37@gmail.com>
2024-06-06 21:56:18 +0100

    Bumps the release version to 0.2.0
    Signed-off-by: Exadra37 <exadra37@gmail.com>

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule ElixirScribe.MixProject do
     [
       app: :elixir_scribe,
       name: "Elixir Scribe",
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: @elixir_requirement,
       start_permanent: Mix.env() == :prod,
       homepage_url: @scm_url,
