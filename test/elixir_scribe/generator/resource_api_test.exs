aa28731492f28a3a47ef1ddb42fa5f4d4bd4aa20
Exadra37 <exadra37@gmail.com>
2024-08-21 16:05:16 +0100

    --wip-- [skip ci]

--- test/elixir_scribe/generator/resource_api_test.exs
+++ test/elixir_scribe/generator/resource_api_test.exs
@@ -10,18 +10,6 @@ defmodule ElixirScribe.Generator.Domain.ResourceAPITest do
 
   # INFO: Tests in the API module only care about testing the function can be invoked and that the API contract is respected (for now only guards and pattern matching). The unit tests for the functionality are done in their respective modules
 
-  describe "parse_args/1" do
-    test "can be invoked" do
-      assert {_parsed_args, _opts, _invalid_args} = MixAPI.parse_cli_command([])
-    end
-
-    test "raises a FunctionClauseError when the argument isn't a list" do
-      assert_raise FunctionClauseError, ~r/^no function clause matching in.*$/s, fn ->
-        MixAPI.parse_cli_command(%{})
-      end
-    end
-  end
-
   describe "build_domain_resource_contract/2" do
     test "can be invoked" do
       args = ["Blog", "Post", "posts", "title:string", "desc:string"]
---
+++ test/elixir_scribe/mix/mix_api_test.exs
@@ -0,0 +1,18 @@
+defmodule ElixirScribe.MixAPITest do
+  alias ElixirScribe.MixAPI
+  use ElixirScribe.BaseCase, async: true
+
+  # INFO: Tests in the API module only care about testing the function can be invoked and that the API contract is respected (for now only guards and pattern matching). The unit tests for the functionality are done in their respective modules
+
+  describe "parse_cli_command/1" do
+    test "can be invoked" do
+      assert {_parsed_args, _opts, _invalid_args} = MixAPI.parse_cli_command([])
+    end
+
+    test "raises a FunctionClauseError when the argument isn't a list" do
+      assert_raise FunctionClauseError, ~r/^no function clause matching in.*$/s, fn ->
+        MixAPI.parse_cli_command(%{})
+      end
+    end
+  end
+end
